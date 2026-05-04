muzammilar/yugabyte-db

commit 5925ba294fcf7d5367767f3605cf3154b0f882cb

[#26270] YSQL: Simplify OID design by removing OID global lists in connection manager
Summary:
This diff removes the usage of global OID list used to store database and user OID entries. All the information will be now persisted in the route object itself.

**Old Behaviour:**

  # 2 Global OID lists are maintained in order to store the OID details of a user and database.
  # The OID entry is referenced by the route for creating a pool corresponding to referenced user OID and database OID.
  # Whenever a new user or database tries to connect, the OID packet returned by the server during authentication is used to create a new entry in either user or database OID global list if it does not exist.
  # This same entry is being referred to by route struct field’s yb_database_entry & yb_user_entry.
  # If a particular user or database is dropped, there are some scenarios in which the connection manager can know a particular user or database OID does not exist. In those cases the corresponding user or database entry in the global list is marked as invalid.
  # To check whether a route is valid or not ? Connection manager checks the status of database and user entry referenced by that route, whether they are valid or not.
  # Every time any entry to be searched, modified or added in above 2 lists a lock is acquired on the global list which could be very expensive and hard to avoid due to race conditions. D41022.

**Note**

  # It’s difficult for connection managers to get real time information about whether a user or database OID exists.
  # Whenever there is no activity in a pool i.e no physical connection and logical connections are there, connection manager automatically deletes the pool irrespective of whether the user or database referenced by those routes exists or not.
  # It’s redundant to maintain the above 2 lists. Instead everything can be maintained inside the route/pool object itself.

**New Behaviour**

src/odyssey/sources/router.c
@@ -324,12 +324,46 @@ int od_router_expire(od_router_t *router, od_list_t *expire_list)
 	return count;
 }
 
+/* Mark the route as inactive for the provided db oid or user oid */
+static inline int yb_mark_route_inactive(od_route_t *route, void **argv)
+{
+	int *db_oid = argv[0];
+	int *user_oid = argv[1];
+
+	od_route_lock(route);
+
+	if (*db_oid != -1 && *db_oid == route->id.yb_db_oid) {
+		route->status = YB_ROUTE_INACTIVE;
+		od_route_unlock(route);
+		return 0;
+	}
+
+	if (*user_oid != -1 && *user_oid == route->id.yb_user_oid) {
+		route->status = YB_ROUTE_INACTIVE;
+		od_route_unlock(route);
+		return 0;
+	}
+
+	od_route_unlock(route);
+
+	return 0;
+}
+
+void yb_mark_routes_inactive(od_router_t *router, int db_oid, int user_oid)
+{
+	void *argv[] = { &db_oid, &user_oid };
+	od_router_foreach(router, yb_mark_route_inactive, argv);
+}
+
 static inline int od_router_gc_cb(od_route_t *route, void **argv)
 {
 	od_route_pool_t *pool = argv[0];
 	od_instance_t *instance = argv[1];
 	int index = route->id.yb_stats_index;
 	od_route_lock(route);
+
+	if (route->status == YB_ROUTE_INACTIVE)
+		goto clean;
 
 	if (od_server_pool_total(&route->server_pool) > 0 ||
 	    od_client_pool_total(&route->client_pool) > 0)
@@ -338,6 +372,7 @@ static inline int od_router_gc_cb(od_route_t *route, void **argv)
 	if (!od_route_is_dynamic(route) && !route->rule->obsolete)
 		goto done;
 
+clean:
 	/* remove route from route pool */
 	assert(pool->count > 0);
 	pool->count--;
@@ -386,10 +421,44 @@ void od_router_stat(od_router_t *router, uint64_t prev_time_us,
 	od_router_unlock(router);
 }
 
+static inline int yb_update_name(od_route_t *route, void **argv)
+{
+	int *obj_type = argv[0];
+	int *oid = argv[1];
+	const char *name = argv[2];
+	od_route_lock(route);
+	if (*obj_type == YB_DATABASE && route->id.yb_db_oid == *oid) {
+		strcpy(route->yb_database_name, name);
+		route->yb_database_name_len = strlen(name);
+	} else if (*obj_type == YB_USER && route->id.yb_user_oid == *oid) {
+		strcpy(route->yb_user_name, name);
+		route->yb_user_name_len = strlen(name);
+	}
+	od_route_unlock(route);
+	return 0;
+}
+
+/*
+* Update the name of the database or user in all the routes created using
+* given database_oid or user_oid.
+*/
+void yb_route_pool_update_name(od_router_t *router, int obj_type, int oid,
+				  char *name)
+{
+	void *argv[] = { &obj_type, &oid, name };
+	/*
+	 * Restrict from using od_router_foreach as it acquires lock on router object.
+	 * This function is called from od_router_route which already acquires lock
+	 * on router object.
+	 */
+	od_route_pool_foreach(&router->route_pool, yb_update_name, argv);
+}
+
 od_router_status_t od_router_route(od_router_t *router, od_client_t *client)
 {
 	kiwi_be_startup_t *startup = &client->startup;
 	od_instance_t *instance = router->global->instance;
+	bool is_auth_backend = client->yb_is_authenticating;
 
 	/* match route */
 	assert(startup->database.value_len);
@@ -509,12 +578,65 @@ od_router_status_t od_router_route(od_router_t *router, od_client_t *client)
 	od_route_t *route;
 	route = od_route_pool_match(&router->route_pool, &id, rule);
 	if (route == NULL) {
+		if (router->route_pool.count >= instance->config.yb_max_pools) {
+			od_router_unlock(router);
+			od_error(
+				&instance->logger, "routing", client, NULL,
+				"Limit reached to create maximum number of pools");
+			return OD_ROUTER_ERROR;
+		}
 		route = od_route_pool_new(&router->route_pool, &id, rule);
 		if (route == NULL) {
 			od_router_unlock(router);
 			return OD_ROUTER_ERROR;
 		}
+		od_log(&instance->logger, "routing", NULL, NULL,
+		       "new route: %s %s", startup->database.value, startup->user.value);
+		if (!is_auth_backend &&
+			(id.yb_db_oid == YB_CTRL_CONN_OID || id.yb_user_oid == YB_CTRL_CONN_OID)) {
+
+				// Set the user and database of control connection pool to "yugabyte"
+				// and "yugabyte" respectively for purpose of creating backends for
+				// auth pass through authentication in control connection pool.
+				strcpy(route->yb_database_name, YB_CTRL_CONN_DB_NAME);
+				route->yb_database_name_len = strlen(YB_CTRL_CONN_DB_NAME);
+
+				strcpy(route->yb_user_name, YB_CTRL_CONN_USER_NAME);
+				route->yb_user_name_len = strlen(YB_CTRL_CONN_USER_NAME);
+		}
+		else {
+			strcpy(route->yb_database_name, startup->database.value);
+			route->yb_database_name_len = strlen(startup->database.value);
+
+			strcpy(route->yb_user_name, startup->user.value);
+			route->yb_user_name_len = strlen(startup->user.value);
+		}
 	}
+	else
+	{
+		if (route->id.yb_db_oid > YB_CTRL_CONN_OID &&
+			strcmp(route->yb_database_name, startup->database.value) != 0) {
+			/*
+			 * The database has been renamed.
+			 * We need to update the name in all the routes created using given
+			 * database_oid.
+			 */
+			yb_route_pool_update_name(router, YB_DATABASE, client->yb_db_oid,
+						  startup->database.value);
+		}
+
+		if (route->id.yb_user_oid > YB_CTRL_CONN_OID &&
+			strcmp(route->yb_user_name, startup->user.value) != 0) {
+			/*
+			 * The user has been renamed.
+			 * We need to update the name in all the routes created using given
+			 * user_oid.
+			 */
+			yb_route_pool_update_name(router, YB_USER, client->yb_user_oid,
+						  startup->user.value);
+		}
+	}
+
 	od_rules_ref(rule);
 
 	od_route_lock(route);

src/odyssey/sources/backend.c
@@ -112,7 +112,8 @@ void od_backend_error(od_server_t *server, char *context, char *data,
 		if (strcmp(error.hint, "Database might have been dropped by another user") == 0)
 		{
 			/* Reset the route and close the client */
-			((od_route_t*)server->route)->yb_database_entry->status = YB_OID_DROPPED;
+			yb_mark_routes_inactive(server->global->router,
+							  ((od_route_t*)server->route)->id.yb_db_oid, -1);
 
 			if (yb_external_client != NULL &&
 			    ((od_client_t *)yb_external_client)->type ==
@@ -132,7 +133,8 @@ void od_backend_error(od_server_t *server, char *context, char *data,
 
 	if (strstr(error.message, "invalid role OID")) {
 		/* Reset the route and close the client */
-		((od_route_t*)server->route)->yb_user_entry->status = YB_OID_DROPPED;
+		yb_mark_routes_inactive(server->global->router, -1,
+						  ((od_route_t*)server->route)->id.yb_user_oid);
 
 		if (yb_external_client != NULL &&
 			((od_client_t *)yb_external_client)->type == OD_POOL_CLIENT_EXTERNAL)
@@ -258,11 +260,11 @@ static inline int od_backend_startup(od_server_t *server,
 	}
 	else
 	{
-		strcpy(db_name, (char *)route->yb_database_entry->name);
-		db_name_len = route->yb_database_entry->name_len + 1;
+		strcpy(db_name, (char *)route->yb_database_name);
+		db_name_len = route->yb_database_name_len + 1;
 
-		strcpy(user_name, (char *)route->yb_user_entry->name);
-		user_name_len = route->yb_user_entry->name_len + 1;
+		strcpy(user_name, (char *)route->yb_user_name);
+		user_name_len = route->yb_user_name_len + 1;
 
 		/*
 		 * The connection between connection manager and the backend is always

src/odyssey/sources/console.c
@@ -557,13 +557,13 @@ static inline int od_console_show_pools_add_cb(od_route_t *route, void **argv)
 	od_route_lock(route);
 	int rc;
 	rc = kiwi_be_write_data_row_add(
-		stream, offset, (char *)route->yb_database_entry->name,
-		(int)route->yb_database_entry->name_len);
+		stream, offset, (char *)route->yb_database_name,
+		(int)route->yb_database_name_len);
 	if (rc == NOT_OK_RESPONSE)
 		goto error;
 	rc = kiwi_be_write_data_row_add(
-		stream, offset, (char *)route->yb_user_entry->name,
-		(int)route->yb_user_entry->name_len);
+		stream, offset, (char *)route->yb_user_name,
+		(int)route->yb_user_name_len);
 	if (rc == NOT_OK_RESPONSE)
 		goto error;
 	char data[64];
@@ -709,8 +709,8 @@ static inline int od_console_show_databases_add_cb(od_route_t *route,
 	od_route_lock(route);
 	int rc;
 	rc = kiwi_be_write_data_row_add(
-		stream, offset, (char *)route->yb_database_entry->name,
-		(int)route->yb_database_entry->name_len);
+		stream, offset, (char *)route->yb_database_name,
+		(int)route->yb_database_name_len);
 	if (rc == NOT_OK_RESPONSE)
 		goto error;
 	od_rule_t *rule = route->rule;
@@ -966,14 +966,14 @@ static inline int od_console_show_servers_server_cb(od_server_t *server,
 		return NOT_OK_RESPONSE;
 	/* user */
 	rc = kiwi_be_write_data_row_add(
-		stream, offset, (char *)route->yb_user_entry->name,
-		(int)route->yb_user_entry->name_len);
+		stream, offset, (char *)route->yb_user_name,
+		(int)route->yb_user_name_len);
 	if (rc == NOT_OK_RESPONSE)
 		return NOT_OK_RESPONSE;
 	/* database */
 	rc = kiwi_be_write_data_row_add(
-		stream, offset, (char *)route->yb_database_entry->name,
-		(int)route->yb_database_entry->name_len);
+		stream, offset, (char *)route->yb_database_name,
+		(int)route->yb_database_name_len);
 	if (rc == NOT_OK_RESPONSE)
 		return NOT_OK_RESPONSE;
 	/* state */
@@ -1094,8 +1094,8 @@ static inline int od_console_show_server_prep_stmt_cb(od_server_t *server,
 			/* user */
 			rc = kiwi_be_write_data_row_add(
 				stream, offset,
-				(char *)route->yb_user_entry->name,
-				(int)route->yb_user_entry->name_len);
+				(char *)route->yb_user_name,
+				(int)route->yb_user_name_len);
 			if (rc == NOT_OK_RESPONSE) {
 				goto error;
 			}
@@ -1103,8 +1103,8 @@ static inline int od_console_show_server_prep_stmt_cb(od_server_t *server,
 			/* database */
 			rc = kiwi_be_write_data_row_add(
 				stream, offset,
-				(char *)route->yb_database_entry->name,
-				(int)route->yb_database_entry->name_len);
+				(char *)route->yb_database_name,
+				(int)route->yb_database_name_len);
 			if (rc == NOT_OK_RESPONSE) {
 				goto error;
 			}

src/odyssey/sources/cron.c
@@ -65,28 +65,28 @@ static int od_cron_stat_cb(od_route_t *route, od_stat_t *current,
 			if (route->id.yb_stats_index == -1) {
 				route->id.yb_stats_index = yb_get_stats_index(
 					instance->yb_stats,
-					route->yb_database_entry->oid,
-					route->yb_user_entry->oid,
+					route->id.yb_db_oid,
+					route->id.yb_user_oid,
 					instance->config.yb_max_pools);
 				if (route->id.yb_stats_index == -1) {
 					od_error(&instance->logger, "stats", NULL, NULL,
 						 "Unable to find the index for (%s, %s) pool",
-						 (char *)route->yb_database_entry->name,
-						 (char *)route->yb_user_entry->name);
+						 (char *)route->yb_database_name,
+						 (char *)route->yb_user_name);
 					return -1;
 				}
 			}
 
 			index = route->id.yb_stats_index;
 			memcpy(instance->yb_stats[index].database_name,
-				(char *)route->yb_database_entry->name,
-				DB_NAME_MAX_LEN - 1);
-			instance->yb_stats[index].database_name[DB_NAME_MAX_LEN - 1] = '\0';
-			instance->yb_stats[index].database_oid = route->yb_database_entry->oid;
-			strncpy(instance->yb_stats[index].user_name,
-				(char *)route->yb_user_entry->name, USER_NAME_MAX_LEN - 1);
-			instance->yb_stats[index].user_name[USER_NAME_MAX_LEN - 1] = '\0';
-			instance->yb_stats[index].user_oid = route->yb_user_entry->oid;
+				(char *)route->yb_database_name,
+				route->yb_database_name_len);
+			instance->yb_stats[index].database_name[route->yb_database_name_len] = '\0';
+			instance->yb_stats[index].database_oid = route->id.yb_db_oid;
+			memcpy(instance->yb_stats[index].user_name,
+				(char *)route->yb_user_name, route->yb_user_name_len);
+			instance->yb_stats[index].user_name[route->yb_user_name_len] = '\0';
+			instance->yb_stats[index].user_oid = route->id.yb_user_oid;
 		}
 
 		od_route_lock(route);

src/odyssey/sources/frontend.c
@@ -324,9 +324,9 @@ od_frontend_attach(od_client_t *client, char *context,
 
 			/* YB: check auth failure status codes to update OID status */
 			if (yb_frontend_error_is_db_does_not_exist(client))
-				((od_route_t *)server->route)->yb_database_entry->status = YB_OID_DROPPED;
+				yb_mark_routes_inactive(router, ((od_route_t *)server->route)->id.yb_db_oid, -1);
 			else if (yb_frontend_error_is_role_does_not_exist(client))
-				((od_route_t *)server->route)->yb_user_entry->status = YB_OID_DROPPED;
+				yb_mark_routes_inactive(router, -1, ((od_route_t *)server->route)->id.yb_user_oid);
 
 			return OD_ESERVER_CONNECT;
 		}
@@ -2549,8 +2549,8 @@ void od_frontend(void *arg)
 			&instance->logger, "auth backend", client, NULL,
 			"invalidate all existing active and idle backends of the route"
 			", with user = %s, db = %s, having %d idle backends and %d active backends",
-			(char *)route->yb_user_entry->name,
-			(char *)route->yb_database_entry->name,
+			(char *)route->yb_user_name,
+			(char *)route->yb_database_name,
 			route->server_pool.count_idle,
 			route->server_pool.count_active);
 		route->max_logical_client_version =

src/odyssey/sources/instance.c
@@ -185,9 +185,6 @@ int od_instance_main(od_instance_t *instance, int argc, char **argv)
 		}
 	}
 
-	rc = yb_oid_list_init(instance);
-	if (rc == -1)
-		goto error;
 
 	yb_read_conf_from_env_var(&router.rules, &instance->config,
 				 &instance->logger);

src/odyssey/sources/route.h
@@ -9,6 +9,11 @@
 
 typedef struct od_route od_route_t;
 
+typedef enum {
+	YB_ROUTE_INACTIVE,
+	YB_ROUTE_ACTIVE,
+} od_route_status_t;
+
 struct od_route {
 	od_rule_t *rule;
 	od_route_id_t id;
@@ -29,8 +34,13 @@ struct od_route {
 	od_error_logger_t *err_logger;
 	bool extra_logging_enabled;
 
-	yb_oid_entry_t* yb_database_entry;
-	yb_oid_entry_t* yb_user_entry;
+	char yb_database_name[DB_NAME_MAX_LEN];
+	int yb_database_name_len;
+
+	char yb_user_name[USER_NAME_MAX_LEN];
+	int yb_user_name_len;
+
+	od_route_status_t status;
 
 	od_list_t link;
 

src/odyssey/sources/route_pool.h
@@ -94,13 +94,7 @@ static inline od_route_t *od_route_pool_new(od_route_pool_t *pool,
 		}
 	}
 
-	route->yb_database_entry = yb_get_oid_obj_entry(YB_DATABASE, id->yb_db_oid);
-	if (route->yb_database_entry == NULL)
-		return NULL;
-
-	route->yb_user_entry = yb_get_oid_obj_entry(YB_USER, id->yb_user_oid);
-	if (route->yb_user_entry == NULL)
-		return NULL;
+	route->status = YB_ROUTE_ACTIVE;
 
 	od_route_lock(route);
 	od_list_append(&pool->list, &route->link);
@@ -247,7 +241,7 @@ od_route_pool_stat_database(od_route_pool_t *pool,
 		od_stat_init(&prev);
 
 		char database[64];
-		strcpy(database, (char *)route->yb_database_entry->name);
+		strcpy(database, (char *)route->yb_database_name);
 		od_route_pool_stat_database_mark(pool, route->id.yb_db_oid,
 						 &current, &prev);
 

commit 82455c2f889cf8dedea6c597ee13fe12b9304009

[#31189] YSQL: ConnMgr to not destroy route in GC if any server is active
Summary:
This diff fixes a race condition introduced in 5925ba294fcf7d5367767f3605cf3154b0f882cb / D42282
where support was added for cleaning up routes on which database/role was renamed or dropped. The
mechanism works as follows:

Whenever we get an error indicating that the database was dropped or role OID doesn't exist, it sets
`route->status` to `YB_ROUTE_INACTIVE` on all affected routes. The cron thread calls `od_router_gc`
on each tick which cleans up these inactive routes.

The problem with the implementation today is that there can still be active connections on that
route when the cron thread executes. If a workload is executing on the database that is dropped,
then there could be active servers for that route. Even more simply, when the server which first hit
the error about database oid not being present and marks the route as invalid, is still active
before cleanup is executed. If cron thread cleans up the route in this window, the server cleanup
itself fails.

The fix for this is to check that there are no active servers on the route also when the invalid
check is done. Only when both of these conditions hold can we safely free the route. We can ignore
the idle connections since we have acquired route level lock and they are always made active after
the lock is obtained.

Test Plan:
Tested manually by adding a 5s sleep after marking the server invalid. In that time interval, the
cron thread fires and cleans up the route which ends up segfaulting the process when the first
thread wakes up. After this fix, the cron thread does not free the route until the server has
successfully transitioned to idle. Note that there are still idle servers on the route but the cron
thread can free the route at that point as explained before

Jenkins: all tests

Reviewers: mkumar, skumar, vikram.damle

Reviewed By: mkumar, skumar

Subscribers: yql

src/odyssey/sources/router.c
@@ -470,7 +470,8 @@ static inline int od_router_gc_cb(od_route_t *route, void **argv)
 	int index = route->id.yb_stats_index;
 	od_route_lock(route);
 
-	if (route->status == YB_ROUTE_INACTIVE)
+	if (route->status == YB_ROUTE_INACTIVE &&
+	    od_server_pool_active(&route->server_pool) == 0)
 		goto clean;
 
 	if (od_server_pool_total(&route->server_pool) > 0 ||